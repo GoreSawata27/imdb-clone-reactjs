2bb81c76e7a70ecf6dd6d26dc4e136e1597f8072
Sawata Gore <sawatagore27@gmail.com>
2023-12-04 09:13:02 +0530

    new

--- src/App.js
+++ src/App.js
@@ -3,17 +3,12 @@ import React from 'react'
 export default function App() {
   return (
     <>
-    <div className="container">
-      <div className="box"></div>
-      <div className="box"></div>
-      <div className="box"></div>
-      <div className="box"></div>
-      <div className="box"></div>
-      <div className="box"></div>
-      <div className="box"></div>
-      <div className="box"></div>
-      <div className="box"></div>
-    </div>
+      <div className="container">
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+      </div>
 
 
     </>
