711a85ce586163d901fc767a9db92162d2187361
Sawata Gore <sawatagore27@gmail.com>
2023-12-04 09:11:18 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App />
+import { BrowserRouter as Router } from "react-router-dom";
+
+import App from "./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
